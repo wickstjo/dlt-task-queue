7d2b56f889e3bbb10edb0e5c66249d01dd6194e7
wickstjo <jfwick@gmail.com>
2021-03-07 16:58:55 +0200

    Added final touches

--- truffle-config.js
+++ truffle-config.js
@@ -3,9 +3,10 @@ module.exports = {
     networks: {
         development: {
             host: '127.0.0.1',
-            port: 8545,
+            port: 8080,
             network_id: '*',
-            gas: 5000000
+            gas: 5000000,
+            websockets: true
         }
     },
     compilers: {
